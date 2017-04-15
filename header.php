4450a46088fb520011c272f319b224635f4bb512
Michael L Parks <github@wmi-inc.com>
2017-04-15 13:54:18 -0400

    Added Responsive Feature

--- header.php
+++ header.php
@@ -7,23 +7,6 @@
     <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
     <link rel="stylesheet" href="./css/header-responsive.css">
     <style media="screen">
-      *{padding: 0px; margin: 0px;}
-      body{font-family: 'Oswald', arial, helvetica, sans-serif; font-size: 18px;}
-      a{text-decoration: none; color: #fff; height: 60px; line-height: 60px; padding: 15px;}
-      header{background-color: #000; color: #fff; width: 100%; height: 105px; line-height: 105px;}
-      .header-brand{width: 33%; float: left; padding-left: 20px;}
-      .header-brand-text{font-size: 36px;}
-        .header-brand-tagline{font-size: 18px; color: #3399fe; font-style: italic;}
-      .header-topnav{width: 63%; float: left; padding-right: 20px; text-align: right;}
-
-      ul li{list-style: none; display: inline-block; height: 60px; line-height: 60px;}
-      ul li a:hover{background-color: #3399fe; color: #fff;}
-
-      @media only screen and (max-width: 1000px) {
-        .header-brand-text{font-size: 30px;}
-        .header-topnav{font-size: 14px;}
-    }
-
 
     </style>
   </head>
